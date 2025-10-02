54b6f3024a071635d03b64238842d9010efe1b7d
ydyryselmira7979-commits <ydyrys.elmira7979@gmail.com>
2025-10-02 14:28:11 +0200

    first.py

--- first.py
+++ first.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
-"""first.py
-Функция sudy_nebo_lichy(cislo) проверяет, чётное или нечётное число,
-и печатает сообщение на чешском: "Číslo X je sudé" / "Číslo X je liché".
-"""
-
+# -*- coding: utf-8 -*-
 def sudy_nebo_lichy(cislo):
-    """Принимает одно целое число 'cislo' и выводит, sudé (чётное) или liché (нечётное)."""
-    try:
-        n = int(cislo)
-    except Exception:
-        print(f"Zadaná hodnota '{cislo}' není celé číslo.")
-        return
-
-    if n % 2 == 0:
-        print(f"Číslo {n} je sudé")
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
     else:
-        print(f"Číslo {n} je liché")
+        print(f"Číslo {cislo} je liché")
 
-if __name__ == "__main__":
-    # Тестовые вызовы, как в задании
-    sudy_nebo_lichy(5)
-    sudy_nebo_lichy(1000000)
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
+
