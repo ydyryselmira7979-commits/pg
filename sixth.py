d769285349d9574705c8cfd0bfc0825664492a78
ydyryselmira7979-commits <ydyrys.elmira7979@gmail.com>
2025-11-14 14:23:29 +0100

    Update sixth.py

--- sixth.py
+++ sixth.py
@@ -1,17 +1,25 @@
 import sys
 import requests
 
-
 def download_url_and_get_all_hrefs(url):
-    hrefs = []
-
-    return hrefs
-
+    r = requests.get(url)
+    if r.status_code != 200:
+        return []
+    t = r.text
+    out = []
+    i = 0
+    while True:
+        i = t.find('<a href="', i)
+        if i == -1:
+            break
+        i += 9
+        j = t.find('"', i)
+        out.append(t[i:j])
+    print(out)
+    return out
 
 if __name__ == "__main__":
     try:
-        url = sys.argv[1]
-        download_url_and_get_all_hrefs(url)
-
+        download_url_and_get_all_hrefs(sys.argv[1])
     except Exception as e:
         print(f"Program skoncil chybou: {e}")
